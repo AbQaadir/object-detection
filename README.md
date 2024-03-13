e8207c115c7af2f68dbd3efe7dd139edf7750401
AbQaadir <qaadir.inbox@gmail.com>
2024-03-14 01:36:33 +0530

    Environemnt creted

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # object-detection
 Sign Language Detection
+
+# How to run
+
+```bash
+conda create -p env python==3.8 -y
+```
+
+```bash
+conda activate C:\obj-detect\object-detection\env
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="signLanguages",
+    version="0.0.0",
+    author="AbQaadir",
+    author_email="qaadir.inbox@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
